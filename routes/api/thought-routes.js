343153b12e4b90f260e21e03838005b81357a033
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-03-04 16:19:25 -0500

    finished routes, now need to make controllers

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,8 +1,28 @@
 const router = require('express').Router();
 
+const {
 
+} = require('../../controllers/thought-controller');
 
+//set up GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought)
 
+//set up GET one, PUT and DELETE by _id at /api/thoughts/:id    
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThoughtById)
+    .delete(deleteThought)
+
+    
+// /api/thoughts/:thoughtId/reactions    
+router
+    .route('/:thoughtId/reaction')
+    .post(addReaction)
+    .delete(deleteReaction)
 
 module.exports = router;
 
